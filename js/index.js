f7f284945275ce8d098ef930bf77c52f80a2df66
OldFatPHP <531218650@qq.com>
2016-09-20 22:21:49 +0800

    更新1
    增加首页index.html，js文件：data.js,highcharts.js,  index.js 和 css文件

---
+++ js/index.js
@@ -0,0 +1,69 @@
+/*
+* @Author: WG
+* @Date:   2016-09-20 21:52:17
+* @Last Modified by:   WG
+* @Last Modified time: 2016-09-20 21:58:49
+*/
+
+'use strict';
+$(function(){
+
+// 用户地域统计
+   $('.user_hometown_table').highcharts({
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: '注册用户地域分布统计表'
+        },
+        subtitle: {
+            text: '截止至2016-9-20'
+        },
+        xAxis: {
+            categories: ['广东', '安徽', '湖南', '哈尔滨', '台湾'],
+            title: {
+                text:'省份'
+            }
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: '注册用户数量（人）',
+                align: 'high'
+            },
+            labels: {
+                overflow: 'justify'
+            }
+        },
+        tooltip: {
+            valueSuffix: '人'
+        },
+        plotOptions: {
+            bar: {
+                dataLabels: {
+                    enabled: true
+                }
+            }
+        },
+        legend: {
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'top',
+            x: -40,
+            y: 100,
+            floating: true,
+            borderWidth: 1,
+            backgroundColor: ((Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'),
+            shadow: true
+        },
+        credits: {
+            enabled: false
+        },
+        series: [{
+            name: '人数',
+            data: [627, 331, 235, 203, 68]
+        }]
+    });
+
+
+});
